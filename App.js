3b451038e806a6678bb20fcd9d973f31221596b1
An <apn2@buffalo.edu>
2023-09-04 16:04:45 -0500

    initialization

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Image, TouchableOpacity, TouchableHighlight, Button,} from 'react-native';
+
 
 export default function App() {
+
+  // comment for user to check in for the day
+  const handlePress = () => {
+    alert('AAAAAAAA')
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: './homescreen.jpg',
+        }}
+      />
+      <Text style={styles.setFontSizeOne}> Welcome to your daily zen space, An :)</Text>
       <StatusBar style="auto" />
+      <Button
+        title='Continue here'
+        style={styles.button}
+        onPress={handlePress}
+      />
     </View>
   );
 }
 
+// this is where the attributes are
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  setFontSizeOne: {
+    fontSize: 20 
   },
 });
